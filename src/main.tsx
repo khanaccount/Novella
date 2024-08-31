df09902831b83b3a514da0b81a24b0a4d76278a9
khanaccount <98438218+khanaccount@users.noreply.github.com>
2024-09-01 04:10:57 +0700

    deploy fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ElfenLied">
       <App />
     </BrowserRouter>
   </React.StrictMode>
